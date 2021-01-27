9989d4103dbfe81a8b818dbc2576ca808f65e2d5
anuj bhatt <anujbhatt023@gmail.com>
2021-01-27 15:05:36 +0530

    initial commit

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+
+  class Layout extends Component{
+
+    render(){
+
+      return (
+           <div class="layout">layout</div>
+      )
+    }
+  }
+
+
+ export default Layout;
