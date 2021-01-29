26713a0187d4a19a63f37737c5e4fa8dd04e206e
sagarmachine <sagarmonkey800@gmail.com>
2021-01-29 13:46:59 +0530

    ripple added

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,14 +1,31 @@
 import React, {Component} from "react"
 import Landing from "./landing/landing"
-
+import mount from "../../assets/images/mount.png"
+import $ from 'jquery'
+import ripples from 'jquery.ripples'
+import {  } from "./layout.css";
   class Layout extends Component{
+    componentDidMount () {
+
+     
+      $('.test').ripples({
+        resolution: 200,
+        perturbance: 0.04,
+      });
+  }
+
 
     render(){
 
+  
       return (
-           <div class="layout">
-               <Landing/>
-           </div>
+
+        <>
+        <div className="test">
+<button>test</button>
+        </div>
+  
+     </>
       )
     }
   }
