6832ade2c5f7b4194e03a4d843ecd7064091908e
anuj bhatt <anujbhatt023@gmail.com>
2021-01-30 21:15:56 +0530

    added welcome page

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -2,7 +2,7 @@ import React, {Component} from "react"
 import Landing from "./landing/landing"
 import Loader from "./loader/loader"
 import Footer from "./footer/footer"
-import mount from "../../assets/images/mount.png"
+import Social from "./fixed/social"
 import $ from 'jquery'
 import ripples from 'jquery.ripples';
 
@@ -27,7 +27,7 @@ class Layout extends Component{
         <div className="landing">
             <Loader loaded={this.state.loaded}/>
             <Landing/>
-            <Footer/>
+            <Social/>
         </div>
       )
     }
